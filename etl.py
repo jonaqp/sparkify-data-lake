fa1226b82a2a437ddeaa3fac6925a57b85776ede
Varun Sharma <varunvsharma@gmail.com>
2019-08-13 23:40:53 +1000

    Add function to create new spark session

--- etl.py
+++ etl.py
@@ -9,11 +9,21 @@ from pyspark.sql.types import IntegerType
 from pyspark.sql.types import TimestampType
 from pyspark.sql.functions import monotonically_increasing_id
 
+
 config = configparser.ConfigParser()
 config.read('dl.cfg')
 
 os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
 os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
+
+
+def create_spark_session():
+    """Return a new Spark Session object configured to work with AWS."""
+    spark = SparkSession \
+        .builder \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .getOrCreate()
+    return spark
 
 
 def main():
