ddee5d3bfd50d04759b3d4c7836f59bde88c6155
Varun Sharma <varunvsharma@gmail.com>
2019-08-13 23:39:45 +1000

    Initial commit

---
+++ etl.py
@@ -0,0 +1,27 @@
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql.functions import from_unixtime
+from pyspark.sql.types import IntegerType
+from pyspark.sql.types import TimestampType
+from pyspark.sql.functions import monotonically_increasing_id
+
+config = configparser.ConfigParser()
+config.read('dl.cfg')
+
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
+
+
+def main():
+    """Start a spark session and process song and log data."""
+    spark = create_spark_session()
+    input_data = "s3a://udacity-dend/"
+    output_data = "s3://ucourse-dend-spark/"
+
+
+if __name__ == "__main__":
+    main()
